08b919df32267e7a45b4887aec1c6d482beb85f5
Dolbocat <Delorkin@yandex.ru>
2022-10-16 20:33:56 +0300

    Доделаю позже уже в рамках саморазвития, основную часть сделал

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,14 +2,108 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Homework5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            bool f = true;
+
+            while (f)
+            {
+                Console.Clear();
+                Console.WriteLine("Тавризов Алексей, Домашняя работа 5");
+                Console.WriteLine("Мои задачи");
+                Console.WriteLine("=================================");
+                Console.WriteLine("1 -> Задача 1");
+                Console.WriteLine("2 -> Задача 2");
+                Console.WriteLine("3 -> Задача 3");
+                Console.WriteLine("4 -> Задача 4");
+                Console.WriteLine("5 -> Задача 5");
+                Console.WriteLine("6 -> Задача 6");
+                Console.WriteLine("0 -> Завершение работы приложения");
+                Console.WriteLine("=================================\n");
+
+                Console.Write("Введите номер задачи: ");
+                int number = int.Parse(Console.ReadLine());
+
+                switch (number)
+                {
+                    case 0:
+                        f = false;
+                        Console.WriteLine("Завершение работы приложения ...");
+                        break;
+                                                
+                    case 1:
+                        Password();
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Swap();
+                        break;
+
+                    case 3:
+                        string a = "корова";
+                        string b = "аоровк";
+                        TextClass.Identical(a, b);
+        
+                                                    
+                        Console.ReadLine();
+                        
+                        break;
+
+                    case 4:
+                        
+                        break;
+
+                    case 5:
+                        
+                        break;
+
+                    case 6:
+                        
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный номер задачи.\nПовторите ввод.");
+                        break;
+
+                }              
+            }
+        }
+        static void Password()
+        {
+            Console.WriteLine(@"Создать программу, которая будет проверять корректность ввода логина.
+Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры,
+при этом цифра не может быть первой.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Введите логин: ");
+            string password = Console.ReadLine();
+            Regex regex = new Regex("^([A-z]|[0-9]){2,10}?$");
+            regex.IsMatch(password);
+            if (regex.IsMatch(password))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Вы ввели корректный логин, добро пожаловать!");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Вы ввели неверный логин!");               
+            }
+        }
+
+        static bool Swap()
+        {
+            return true;
         }
     }
 }
+
--- Homework5/TextClass.cs
+++ Homework5/TextClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace Homework5
 {
     public static class TextClass
     {
-        private static string[] separators = {",",".","-","!","?"," ",";",":"};
+        readonly private static string[] separators = {",",".","-","!","?"," ",";",":"};
 
         public static void PrintWords(string message)
         {
@@ -22,6 +23,78 @@ namespace Homework5
             }
         }
 
+        public static void NoMore(string message,int symbols) // А
+        {            
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= symbols )
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }       
+        }
 
+        public static void RemoveWords(string message,char n) //Б
+        {
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i][words[i].Length - 1] == n)
+                {
+                    words[i] = words[i].Remove(0, words[i].Length);                   
+                }                               
+            }
+            string message2 = string.Join(" ", words);
+            Console.WriteLine(message2);
+        }
+
+        public static void LongWord(string message) //В
+        {
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string count = " ";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > count.Length)
+                {
+                    count = words[i];
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        public static void SBMessage(string message,int symbols) //Г
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= symbols)
+                {
+                    stringBuilder.Append(words[i] + " ");
+                }
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+        public static void Identical(string word1, string word2) // не доделал пока, добью в рамках саморазвития попозже
+        {
+            string word3 = "";
+            for (int i = 0 - 1; i <= word1.Length; i++)
+            {
+                if (word2[i] == word1[i])
+                {
+
+                }
+            }
+            if (word3.Equals(word2))
+            {
+                Console.WriteLine("Слова одинаковы");
+            }
+            else
+            {
+                Console.WriteLine("Слова разные");
+            }
+        }
     }
 }
